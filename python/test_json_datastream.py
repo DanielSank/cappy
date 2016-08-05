92f8f8adfcbc703fd5b20623a3dd527e2b0a41a3
Daniel Sank <sank.daniel@gmail.com>
2016-08-05 00:05:14 -0700

    Improve some imports.

--- python/test_json_datastream.py
+++ python/test_json_datastream.py
@@ -1,22 +1,24 @@
-import cappy
+import stream
 import pytest
+
 
 class TestJsonDataStrem(object):
 
     def test_single_object(self):
-        stream = cappy.JsonDataStream()
-        result = stream.receive("""{"id": 1, "name": "Daniel"}""")
+        s = stream.JsonDataStream()
+        result = s.receive("""{"id": 1, "name": "Daniel"}""")
         assert result == [{'id': 1, 'name': "Daniel"}]
 
     def test_multiple_objects(self):
-        stream = cappy.JsonDataStream()
-        result = stream.receive("""{"id":1, "name": "Daniel"}{"id": 2}""")
+        s = stream.JsonDataStream()
+        result = s.receive("""{"id":1, "name": "Daniel"}{"id": 2}""")
         assert result == [{'id': 1, 'name': "Daniel"}, {'id': 2}]
 
     def test_partial_streams(self):
         messages = """{"id": 1, "name": "Daniel"}{"color": "green"}"""
-        stream = cappy.JsonDataStream()
-        result = stream.receive(messages[0:10])
+        s = stream.JsonDataStream()
+        result = s.receive(messages[0:10])
         assert result == []
-        result = stream.receive(messages[10:])
+        result = s.receive(messages[10:])
         assert result == [{'id': 1, 'name': "Daniel"},{'color': "green"}]
+
